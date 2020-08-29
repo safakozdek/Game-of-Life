eb858baad49c00528db3b455b5b56161de4fef72
safakozdek <safakozdek@gmail.com>
2020-08-29 20:50:17 +0300

    Gosper's Glider Gun added.

--- Code/main.go
+++ Code/main.go
@@ -31,7 +31,14 @@ var (
 	gliderY = []int{2, 3, 1, 2, 3}
 	glider  = [][]int{gliderX, gliderY}
 
-	defaultScreens = [][][]int{empty, block, glider}
+	gliderGunX = []int{1, 1, 2, 2, 11, 11, 11, 12, 12, 13, 13, 14, 14, 15, 16, 16, 17, 17, 17, 18,
+		21, 21, 21, 22, 22, 22, 23, 23, 25, 25, 25, 25, 35, 35, 36, 36}
+	gliderGunY = []int{5, 6, 5, 6, 5, 6, 7, 4, 8, 3, 9, 3, 9, 6, 4, 8, 5, 6, 7, 6,
+		3, 4, 5, 3, 4, 5, 2, 6, 1, 2, 6, 7, 3, 4, 3, 4}
+	gliderGun = [][]int{gliderGunX, gliderGunY}
+
+
+	defaultScreens = [][][]int{empty, block, glider, gliderGun}
 )
 
 type Game struct {
@@ -125,3 +132,4 @@ func main() {
 		log.Fatal(err)
 	}
 }
+
